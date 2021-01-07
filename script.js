dcad7a29f80a9dd0d183d80ccdede1a10f49cbdb
Gustavo Tramontin <mrgustavotramontine@gmail.com.com>
2021-01-07 13:18:06 -0300

    add decrypt functionality

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 class ViginereCypher {
     constructor() {
-        this.letters = ['a', 'b', 'c', 'd', 'e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
+        this.letters = ['a', 'b', 'c', 'd', 'e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']//,'1','2','3','4','5','6','7','8','9','0']
 
         this.key_selector = '.key input'
     }
 
-    encrypt(text) {
+    encrypt(text, textarea=1, decrypt=false) {
         const key = document.querySelector(this.key_selector).value
 
         let encrypted_msg = ''
@@ -23,17 +23,32 @@ class ViginereCypher {
 
             console.log(text_letters_index, key_letters_index)
 
+            if (!decrypt) {
+                encrypted_msg += letter_duplicated[text_letters_index + key_letters_index]
+                console.log(text_letters_index - key_letters_index )
+            } else {
+                console.log(text_letters_index - key_letters_index )
+                const index = text_letters_index - key_letters_index
 
-            encrypted_msg += letter_duplicated[text_letters_index+key_letters_index]
+                if (index < 0) {
+                    encrypted_msg += letter_duplicated[letter_duplicated.length - Math.abs(index)]
+                } else {
+                    encrypted_msg += letter_duplicated[index]
+                }
+            }
 
             i++
         }
 
-        const encrypedt_input = document.querySelectorAll('textarea')[1]
+        const encrypedt_input = document.querySelectorAll('textarea')[textarea]
 
         encrypedt_input.value = encrypted_msg
 
         
+    }
+
+    decrypt(text) { // The way you encrypt is very similar to decrypt then i made the encrypt function decrypt too
+        this.encrypt(text, 0, true)
     }
 
     extend_key(key, length) { // This function is to extend the size of key to the size of the message by repeating the characters
